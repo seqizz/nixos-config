04cb2fae6f4735d8388a57313ccaf326bca998ad
Gürkan Gür <gurkan.gur@innogames.com>
2021-07-13 23:56:40 +0200

    Upgrade kernel

--- machines/innodellix.nix
+++ machines/innodellix.nix
@@ -40,7 +40,8 @@ in
       };
     };
     kernelModules = [ "kvm-intel" "i915" ];
-    kernelPackages = pkgs.linuxPackages_5_12;
+    # kernelPackages = pkgs.linuxPackages_5_12;
+    kernelPackages = pkgs.linuxPackages_5_13;
     kernelParams = [
       "i915.enable_fbc=1"
       "i915.enable_guc=2"
