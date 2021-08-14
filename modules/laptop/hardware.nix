3494342c4d80771d271d0ce5da3d1a4526921d97
Gürkan Gür <gurkan.gur@innogames.com>
2021-08-15 00:22:21 +0200

    Don't use sound from dock

--- modules/laptop/hardware.nix
+++ modules/laptop/hardware.nix
@@ -29,9 +29,9 @@
       support32Bit = true;
       extraModules = [ pkgs.pulseaudio-modules-bt ];
       # @Reference to blacklist any devices' auto-switch
-      # load-module module-switch-on-connect blacklist="myshit"
+      # load-module module-switch-on-connect blacklist=""
       extraConfig = ''
-        load-module module-switch-on-connect
+        load-module module-switch-on-connect blacklist="hdmi|USB Audio"
         load-module module-alsa-card device_id="1" name="usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00" card_name="alsa_card.usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00" namereg_fail=false tsched=yes fixed_latency_range=yes ignore_dB=no deferred_volume=yes use_ucm=yes avoid_resampling=yes card_properties="module-udev-detect.discovered=1" tsched_buffer_size=65536 tsched_buffer_watermark=20000
       '';
     };
