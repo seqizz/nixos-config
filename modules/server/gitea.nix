754452c5cfbf9d681d5ce95079836948798887ac
Gürkan Gür <gurkan.gur@innogames.com>
2022-05-20 15:10:39 +0200

    Adjust gitea settings

--- modules/server/gitea.nix
+++ modules/server/gitea.nix
@@ -9,11 +9,17 @@
     domain = "git.gurkan.in";
     disableRegistration = true;
     settings = {
+      repository = {
+        DEFAULT_REPO_UNITS = "repo.code,repo.releases";
+      };
       server = {
         LANDING_PAGE = "explore";
       };
+      ui = {
+        SHOW_USER_EMAIL = false;
+      };
       "ui.meta" = {
-        DESCRIPTION = "This is where I fork my stuff";
+          DESCRIPTION = "This is where I fork my stuff";
       };
       other = {
         SHOW_FOOTER_VERSION = false;
