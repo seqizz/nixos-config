c9436da1fa4b0e2e38911c327ac5828b75c16f75
Gürkan <gitoperations@gurkan.in>
2022-06-01 22:30:20 +0200

    Enable pipewire

--- modules/laptop/sound.nix
+++ modules/laptop/sound.nix
@@ -1,34 +1,49 @@
 { config, lib, pkgs, ... }:
-let
-  signal_script = pkgs.writeScript "signal_script" ''
-    #!${pkgs.bash}/bin/bash -eu
-    ${pkgs.coreutils}/bin/coreutils --coreutils-prog=sleep 1
-    ${pkgs.dbus}/bin/dbus-send --system --type=signal / "$1"
-  '';
-in
+# @Reference see below
+# let
+  # signal_script = pkgs.writeScript "signal_script" ''
+    # #!${pkgs.bash}/bin/bash -eu
+    # ${pkgs.coreutils}/bin/coreutils --coreutils-prog=sleep 1
+    # ${pkgs.dbus}/bin/dbus-send --system --type=signal / "$1"
+  # '';
+# in
 {
-  sound.enable = true;
+  # @Reference for tunables of pulseaudio
+  # sound.enable = true;
+  # hardware = {
+    # pulseaudio = {
+      # enable = true;
+      # package = pkgs.pulseaudioFull;
+      # support32Bit = true;
+      # extraModules = [ pkgs.pulseaudio-modules-bt ];
+      # # @Reference to blacklist any devices' auto-switch
+      # # load-module module-switch-on-connect blacklist=""
+      # extraConfig = ''
+        # load-module module-switch-on-connect blacklist="USB|Dock"
+        # load-module module-alsa-card device_id="1" name="usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00" card_name="alsa_card.usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00" namereg_fail=false tsched=yes fixed_latency_range=yes ignore_dB=no deferred_volume=yes use_ucm=yes avoid_resampling=yes card_properties="module-udev-detect.discovered=1" tsched_buffer_size=65536 tsched_buffer_watermark=20000
+      # '';
+    # };
+  # };
 
-  hardware = {
-    # WTH is pulseaudio under hardware section?
-    pulseaudio = {
-      enable = true;
-      package = pkgs.pulseaudioFull;
-      support32Bit = true;
-      extraModules = [ pkgs.pulseaudio-modules-bt ];
-      # @Reference to blacklist any devices' auto-switch
-      # load-module module-switch-on-connect blacklist=""
-      extraConfig = ''
-        load-module module-switch-on-connect blacklist="USB|Dock"
-        load-module module-alsa-card device_id="1" name="usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00" card_name="alsa_card.usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00" namereg_fail=false tsched=yes fixed_latency_range=yes ignore_dB=no deferred_volume=yes use_ucm=yes avoid_resampling=yes card_properties="module-udev-detect.discovered=1" tsched_buffer_size=65536 tsched_buffer_watermark=20000
-      '';
-    };
-  };
-
-  services.udev.extraRules = lib.mkMerge [
-    # Emit a new DBUS signal, if new sound device added
-    ''ACTION=="add",	SUBSYSTEM=="sound", ENV{ID_TYPE}=="audio", RUN+="${signal_script} org.custom.gurkan.sound_device_added"''
-    ''ACTION=="remove",	SUBSYSTEM=="sound", ENV{DEVPATH}=="*/card[0-9]", ENV{ID_TYPE}=="audio", RUN+="${signal_script} org.custom.gurkan.sound_device_removed"''
+  security.rtkit.enable = true;
+  environment.systemPackages = with pkgs; [
+    pulseaudio
   ];
-}
 
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      config.pipewire-pulse."context.exec" = [
+        { "path" = "${pkgs.pulseaudio}/bin/pactl"; "args" = "load-module module-switch-on-connect blacklist=\"USB|Dock\""; }
+      ];
+    };
+    # @Reference: Emit a new DBUS signal, if new sound device added
+    # udev.extraRules = lib.mkMerge [
+      # ''ACTION=="add",	SUBSYSTEM=="sound", ENV{ID_TYPE}=="audio", RUN+="${signal_script} org.custom.gurkan.sound_device_added"''
+      # ''ACTION=="remove",	SUBSYSTEM=="sound", ENV{DEVPATH}=="*/card[0-9]", ENV{ID_TYPE}=="audio", RUN+="${signal_script} org.custom.gurkan.sound_device_removed"''
+    # ];
+  };
+}
