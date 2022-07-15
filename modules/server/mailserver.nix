4bfe0051902b557accac5fd4b28d825bf6e3fdde
Gürkan <gitoperations@gurkan.in>
2022-07-15 16:37:19 +0200

    22.05 changes for server

--- modules/server/mailserver.nix
+++ modules/server/mailserver.nix
@@ -8,8 +8,8 @@ in
 {
   imports = [
     (builtins.fetchTarball {
-      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/nixos-21.11/nixos-mailserver-nixos-21.11.tar.gz";
-      sha256 = "1i56llz037x416bw698v8j6arvv622qc0vsycd20lx3yx8n77n44";
+      url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/nixos-22.05/nixos-mailserver-nixos-22.05.tar.gz";
+      sha256 = "0csx2i8p7gbis0n5aqpm57z5f9cd8n9yabq04bg1h4mkfcf7mpl6";
     })
   ];
 
