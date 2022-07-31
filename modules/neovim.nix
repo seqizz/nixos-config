a8db30e51ad50105a7fe525bf3c909c039467ead
Gürkan <gitoperations@gurkan.in>
2022-07-31 22:46:03 +0200

    Update neovim modules

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -19,6 +19,33 @@ let
         sha256 = "1sd1i6z2q864jmrcmq67g7nqdj4v9ys1px5vw8p37qd9vqs3iall";
       };
     };
+    vim-yadi = buildVimPluginFrom2Nix {
+      name = "vim-yadi";
+      src = pkgs.fetchFromGitHub {
+        owner = "timakro";
+        repo = "vim-yadi";
+        rev = "d868366707bfc966f856347828607f92bc5cd9fb";
+        sha256 = "0c34y7w31vg2qijprhnd0dakmqasaiflrkh54iv8shn79l7cvhsm";
+      };
+    };
+    nvim-transparent = buildVimPluginFrom2Nix {
+      name = "nvim-transparent";
+      src = pkgs.fetchFromGitHub {
+        owner = "xiyaowong";
+        repo = "nvim-transparent";
+        rev = "1a3d7d3b7670fecbbfddd3fc999ddea5862ac3c2";
+        sha256 = "0w8ya9fn9gfqbq6dn5wxkl9v6a9i1p8v691a9x65mfm0v7744nd2";
+      };
+    };
+    linediff = buildVimPluginFrom2Nix {
+      name = "linediff";
+      src = pkgs.fetchFromGitHub {
+        owner = "AndrewRadev";
+        repo = "linediff.vim";
+        rev = "c7710dbc59881b038ca064b6c54fe482303e8304";
+        sha256 = "1qm2fphap3g9lc5kqyhpzqaq21r10bd1c9mlir3rss13i9aqhkl0";
+      };
+    };
   };
 in {
   environment.systemPackages = with pkgs; [
@@ -39,17 +66,18 @@ in {
       packages.myVimPackages = with pkgs.vimPlugins // customVimPlugins; {
         start = [
           # nvim-ts-rainbow # rainbow paranthesis - build fails / wants ctags
+          airline
+          coc-lua
           coc-nvim
           coc-pyright
-          coc-lua
-          airline
           colorizer
           impatient-nvim
-          indentLine
+          indent-blankline-nvim-lua
           limelight-vim
+          linediff
           nerdcommenter # quick comment
           nur.repos.m15a.vimExtraPlugins.pretty-fold-nvim
-          nvim-treesitter
+          nvim-transparent
           tagbar # sidebar
           telescope-nvim
           telescope-zoxide
@@ -67,6 +95,7 @@ in {
           vim-oscyank
           vim-puppet-4tabs
           vim-trailing-whitespace
+          vim-yadi
           vimwiki
         ];
       };
