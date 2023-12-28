af289ec659bdebe7951e09241319464bd02db675
Gürkan <gitoperations@gurkan.in>
2023-12-28 21:38:19 +0100

    fix: Tell all units to wait for shared folder

--- modules/server/shared-folder.nix
+++ modules/server/shared-folder.nix
@@ -19,9 +19,32 @@
       ExecStart = "${pkgs.cryptsetup}/bin/cryptsetup open /encrypted-file encVol -d /root/.decrypt-file";
       ExecStop = "${pkgs.cryptsetup}/bin/cryptsetup close encVol";
     };
-
   };
+
+  # And now the depending services
+  systemd.services = {
+    "rustypaste".unitConfig.RequiresMountsFor = "/shared";
+    "syncthing".unitConfig.RequiresMountsFor = "/shared";
+    "dovecot2".unitConfig.RequiresMountsFor = "/shared";
+    "opendkim".unitConfig.RequiresMountsFor = "/shared";
+    "postfix".unitConfig.RequiresMountsFor = "/shared";
+    "bind".unitConfig.RequiresMountsFor = "/shared";
+    "remark-gurkanin".unitConfig.RequiresMountsFor = "/shared";
+    "remark-siktirin".unitConfig.RequiresMountsFor = "/shared";
+    "nginx".unitConfig.RequiresMountsFor = "/shared";
+    "acme-gurkan.in".unitConfig.RequiresMountsFor = "/shared";
+    "acme-mail.gurkan.in".unitConfig.RequiresMountsFor = "/shared";
+    "acme-siktir.in".unitConfig.RequiresMountsFor = "/shared";
+  };
+
   fileSystems."/shared" = {
+    # @Bug not working with encrypted "files" as block devices
+    # encrypted = {
+      # enable = true;
+      # label = "encVol";
+      # keyFile = "/root/.decrypt-file";
+      # blkDev = "/encrypted-file";
+    # };
     device = "/dev/mapper/encVol";
     fsType = "btrfs";
     options = ["noatime"];
